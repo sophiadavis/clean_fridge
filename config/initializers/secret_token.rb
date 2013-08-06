b9f2c0e607d5c441b91ad5bc6e2d3cbab95235e7
Sophia Davis <sophia@Sophias-MacBook-Air.local>
2013-08-06 00:38:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CleanFridge::Application.config.secret_token = 'ca3b62c018a3e8aa2e43adaf0bbd9ea5c7c836313abfd0787e06b183f6ef06be3aabecb308496a1e9a5b3662c7feb71029e21909b078461ae69f51633e53a37b'
