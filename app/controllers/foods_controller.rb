a58155cf4d790a7238c4b3c82fb58db1264f3ba5
Sophia Davis <scdgrapefruit@gmail.com>
2013-08-16 17:33:54 -0500

    fix delete count

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -14,22 +14,12 @@ class FoodsController < ApplicationController
   
   def destroy
   	@food = Food.find_by_id(params[:id])
-#   	@user = User.find(session[:remember_token])
   	@user = @food.fridge.user
-  	debugger
   	@food.destroy
-#   	Food.destroy(params[:id])
-#   	@food.destroy WHY DIDN'T THIS WORK?
 	delete_count = @user.delete_count
 	@user.update_attribute(:delete_count, delete_count + 1)
-# 	@user.save
-# 	increment_delete_count
-	debugger
-#   	flash[:error] = @user.name
+	sign_in @user
 	redirect_to @user
-#  	redirect_back_or root_path #GET THIS TO WORK
   end
 
 end
-
-#      \"hyRj274QDI0rqapf0exOGQ\"
